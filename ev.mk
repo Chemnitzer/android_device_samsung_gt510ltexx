fb3f910b9801ecf0cb4dd7e2ad2790b941d6ce95
Sean Hoyt <seanhoyt963@gmail.com>
2016-07-04 23:26:59 -0700

    Update the codename

--- ev.mk
+++ ev.mk
@@ -39,7 +39,7 @@ PRODUCT_MODEL := SM-T555
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=gt510ltexx TARGET_DEVICE=gt510lte
 
 # Set up the product codename, build version & MOTD.
-PRODUCT_CODENAME := sedo
+PRODUCT_CODENAME := carus
 
 PRODUCT_MOTD :="\n\n\n--------------------MESSAGE---------------------\nThank you for choosing Evervolv for your Samsung Galaxy A Tablet\nPlease visit us at \#evervolv on irc.freenode.net\nFollow @preludedrew for the latest Evervolv updates\nGet the latest rom at evervolv.com\n------------------------------------------------\n"
 
